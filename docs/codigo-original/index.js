57dad85771f7914526b8daac2272bd5679763299
Jorge Feldmann <eusoujf@hotmail.com>
2016-08-04 17:56:39 -0300

    DOCS: renomenado pasta codigo original

--- docs/codigo-original/index.js
+++ docs/codigo-original/index.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var app = express();
 var Sequelize = require('sequelize');
-var sequelize = new Sequelize('pokemons', null, null, {
+var sequelize = new Sequelize('elements', null, null, {
 	dialect: 'sqlite'
 });
 var bodyParser = require('body-parser');
@@ -13,7 +13,7 @@ app.listen(3000, function () {
 	console.log('Listening on http://localhost:3000');
 });
 
-var Pokemon = sequelize.define('pokemon', {
+var Model = sequelize.define('element', {
 	name: {
 		type: Sequelize.STRING,
 		allowNull: false
@@ -29,34 +29,34 @@ var Pokemon = sequelize.define('pokemon', {
 	}
 });
 
-Pokemon.sync({force: true}).then(function () {
+Model.sync({force: true}).then(function () {
 	console.log('Model is ready!');
 });
 
-app.get('/get-pokemons', function (req, res) {
-	Pokemon.findAll()
-		.then(function listOfPokemons(pokemons){
-			res.send(pokemons);
+app.get('/get-elements', function (req, res) {
+	Model.findAll()
+		.then(function listOfModels(elements){
+			res.send(elements);
 		})
 });
 
-app.put('/create-pokemons', function (req, res) {
-	Pokemon.create(req.body)
-		.then(function sendPokemon(pokemon){
-			res.send(pokemon)
+app.put('/create-elements', function (req, res) {
+	Model.create(req.body)
+		.then(function sendModel(element){
+			res.send(element)
 		})
 });
 
-app.post('/buy-pokemons', function (req, res) {
-	Pokemon.findOne({
+app.post('/buy-elements', function (req, res) {
+	Model.findOne({
 		where: {
 			name: req.body.name
 		}
 	})
-	.then(function(pokemon) {
-		if (pokemon.stock < req.body.quantity) {
+	.then(function(element) {
+		if (element.stock < req.body.quantity) {
 			return res.status(400).send({
-				error: 'Not enought ' + pokemon.name + ' in stock: ' + pokemon.stock
+				error: 'Not enought ' + element.name + ' in stock: ' + element.stock
 			})
 		}
 
@@ -65,14 +65,14 @@ app.post('/buy-pokemons', function (req, res) {
 			method: 'POST',
 			json: {
 				api_key: "ak_test_WHgSu2XFmvoopAZMetV3LfA2RfEEQg",
-				amount: pokemon.price * req.body.quantity * 100,
+				amount: element.price * req.body.quantity * 100,
 				card_number: "4024007138010896",
 				card_expiration_date: "1050",
 				card_holder_name: "Ash Ketchum",
 				card_cvv: "123",
 				metadata: {
 					product: 'pokemon',
-					name: pokemon.name,
+					name: element.name,
 					quantity: req.body.quantity
 				}
 			}
@@ -80,9 +80,9 @@ app.post('/buy-pokemons', function (req, res) {
 		.then(function (body){
 			if (body.status == 'paid') {
 
-				pokemon.stock = pokemon.stock - req.body.quantity;
-				pokemon.save()
-					.then(function(pokemon) {
+				element.stock = element.stock - req.body.quantity;
+				element.save()
+					.then(function(element) {
 						res.send(body);
 					})
 			}
