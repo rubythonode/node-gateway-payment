5c05719e3c9a1042a6f92c5b33359189e32d7065
Jorge Feldmann <eusoujf@hotmail.com>
2016-08-23 02:56:19 -0300

    TESTS: normalizing stock

--- tests/functional/tests/pokemons.specs.js
+++ tests/functional/tests/pokemons.specs.js
@@ -8,8 +8,8 @@ let recurso = config.gerarRecurso ( 'pokemons' );
 
 let model = {
 	name : 'Pikachu',
-	price : utils.gerarNumero ( 2 ),
-	stock : 10
+	price : 1,
+	stock : 1
 };
 
 let buyModel = {
@@ -25,7 +25,7 @@ let buyUnavailableModel = {
 describe ( 'Pokemons' , function () {
 
 	it ( '[CREATE_ONE] deve criar um registro' , function ( done ) {
-			recurso
+			return recurso
 			.put ()
 			.send ( model )
 			.expect ( 200 )
@@ -33,7 +33,7 @@ describe ( 'Pokemons' , function () {
 		});
 
 		it ( '[GET_ALL] deve obter todos os registros' , function ( done ) {
-			recurso
+			return recurso
 			.get ()
 			.expect ( 200 )
 			.expect ( utils.theresDataOnArray , done )
@@ -41,7 +41,7 @@ describe ( 'Pokemons' , function () {
 		});
 
 	it ( '[BUY_ONE] deve efetuar uma transação com registro existente' , function ( done ) {
-			recurso
+			return recurso
 			.post ( 'buy' )
 			.send ( buyModel )
 			.expect ( 200 )
@@ -50,7 +50,7 @@ describe ( 'Pokemons' , function () {
 	);
 
 	it ( '[BUY_ONE] NÃO deve efetuar uma transação com estoque 0' , function ( done ) {
-			recurso
+			return recurso
 			.post ( 'buy' )
 			.send ( buyModel )
 			.expect ( 400 )
@@ -59,7 +59,7 @@ describe ( 'Pokemons' , function () {
 	);
 
 	it ( '[BUY_ONE] NÃO deve efetuar uma transação com registro inexistente' , function ( done ) {
-			recurso
+			return recurso
 			.post ( 'buy' )
 			.send ( buyUnavailableModel )
 			.expect ( 404 )
